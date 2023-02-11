b87dd3de5d3e1431280fdc535b631d94fbcb6292
tannukumari14 <94106847+tannukumari14@users.noreply.github.com>
2023-02-11 22:24:10 +0530

    Add files via upload

---
+++ question.py
@@ -0,0 +1,60 @@
+# print("!" * True)
+# print("!" * False)
+# print("10" * False)
+# print("10" * 10)
+# print("#" * 10)
+
+# name=input("enter your name:-")
+# print(name)
+# color=input("enter the color:-")
+# print(name + "like" + color)
+
+# birth=(input("enter the birth year"))
+# age=2022-int(birth)
+# print(age)
+# print(type(birth))
+
+# weigth_rupees=int(input("enter the weigth :"))
+# weigth_kilo=weigth_rupees*0.45
+# print(weigth_kilo)
+
+# a='''hi i'm tanu kumari'''
+# s=string replace()
+
+# a='tanu'," "'is a bad girl'
+# c=""
+# for i in a:
+#     c+=i
+# print(c)
+
+# a="fgcshdb dftyuush"
+# print(a[1:-1])
+
+# a='tanu'" ,"'is a bad girl'
+# c=""
+# for i in a:
+#     c+=i
+# print('"',c,'"')
+
+# a='tanu'" "'is a bad girl'
+# print('"',a,'"')
+
+# a="tanu"
+# b="kumari"
+# format string
+# c=f'{a}[{b}]is a coder'
+# print(c)
+# print(a+'['+b+']''is a girl')
+
+# a="Suhsiodk ghduiw  HUHH "
+# c=a.upper()
+# print(c)
+# print(a)
+# print(a.find('yeh'))
+
+x = 1  
+while True:  
+    if x % 5 == 0:  
+        break  
+    print(x)   
+    x += 1
